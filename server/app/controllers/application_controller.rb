a4522aef2cfc679d95ba7d2dd46ccdb3f28c2fea
Harrison Shoebridge <harrison@theshoebridges.com>
2020-07-27 12:14:43 +1000

    Proper username and password

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => "admin", :password => "admin"
+  http_basic_authenticate_with  :name => (Rails.application.credentials[:admin_username] or "admin"), :password => (Rails.application.credentials[:admin_password] or "admin")
 end
